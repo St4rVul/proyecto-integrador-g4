c72b713bc592d016b3d4da0c45e76efd483fa7c0
Emilson <96276414+Hemylson@users.noreply.github.com>
2025-02-22 16:09:48 -0500

    Revert "Cambios en carpetas y agregando información."

--- sesion-1-final/src/layout/header/Header.jsx
+++ sesion-1-final/src/layout/header/Header.jsx
@@ -3,23 +3,11 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <img src="/miniBlanco.svg" alt="Logo" className="logo" />
-      <nav className="nav">
+    <header>
+      <nav>
         <NavLink to="/" end>
           Inicio
         </NavLink>
-        <div className="dropdown">
-          <NavLink to="/enfermedades" end>
-            Enfermedades
-          </NavLink>
-          <div className="dropdown-content">
-            <NavLink to="/enfermedades/cirrosis">Cirrosis</NavLink>
-            <NavLink to="/enfermedades/cancer-hepatico">Cáncer Hepático</NavLink>
-            <NavLink to="/enfermedades/hepatitis">Hepatitis</NavLink>
-            <NavLink to="/enfermedades/higado-graso">Hígado Graso</NavLink>
-          </div>
-        </div>
         <NavLink to="/quiz" end>
           Quiz
         </NavLink>

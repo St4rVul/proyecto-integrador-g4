ba3ee2a7b39d6aee547d8ebebf198e5a77361f50
Emilson <emilson.cossio@gmail.com>
2025-02-21 23:44:16 -0500

    Cambios en el header y el ícono en la pestaña del navegador

--- sesion-1-final/src/layout/header/Header.jsx
+++ sesion-1-final/src/layout/header/Header.jsx
@@ -3,11 +3,23 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <img src="/miniBlanco.svg" alt="Logo" className="logo" />
+      <nav className="nav">
         <NavLink to="/" end>
           Inicio
         </NavLink>
+        <div className="dropdown">
+          <NavLink to="/enfermedades" end>
+            Enfermedades
+          </NavLink>
+          <div className="dropdown-content">
+            <NavLink to="/enfermedades/cirrosis">Cirrosis</NavLink>
+            <NavLink to="/enfermedades/cancer-hepatico">Cáncer Hepático</NavLink>
+            <NavLink to="/enfermedades/hepatitis">Hepatitis</NavLink>
+            <NavLink to="/enfermedades/higado-graso">Hígado Graso</NavLink>
+          </div>
+        </div>
         <NavLink to="/quiz" end>
           Quiz
         </NavLink>
